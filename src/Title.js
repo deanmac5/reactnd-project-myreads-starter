3e1706ef0ae3dc01a94802d80b0485714155f2b2
Dean Macaulay <dean@magpies.net>
2018-05-07 10:49:34 +1000

    feat: Added title as a component

---
+++ src/Title.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+class Title extends Component {
+
+    render() {
+        return (
+            <div className="list-books-title">
+                <h1>Dean Reads</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Title
