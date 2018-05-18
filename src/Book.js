5796e43f984d430bc977f820ae754936ba7e767c
Dean Macaulay <deanmac5@gmail.com>
2018-05-18 14:06:19 +1000

    fix: pages render even if book has no image

--- src/Book.js
+++ src/Book.js
@@ -8,18 +8,22 @@ class Book extends Component {
         return authors ? authors.join(', ') : 'Unknown';
     }
 
+    hasThumbail = (book) => {
+       
+        return book.imageLinks ?  true : false;
+    }
+
     render() {
         const authors = this.getAuthors(this.props.book.authors);
+       
 
         return (
             <div className="book">
                 <div className="book-top">
-
                     <div className="book-cover">
-                        <img style={{ width: 128, height: 192 }} alt={this.props.book.title} src={this.props.book.imageLinks.thumbnail}></img>
+                    {this.hasThumbail(this.props.book) ? <img style={{ width: 128, height: 192 }} alt={this.props.book.title} src={this.props.book.imageLinks.thumbnail}></img>: <div>No image</div>}
                     </div>
-                    <BookShelfChanger book={this.props.book} updateShelf={this.props.updateShelf} />
-                   
+                    <BookShelfChanger book={this.props.book} updateShelf={this.props.updateShelf} />              
                 </div>
                 <div className="book-title">{this.props.book.title}</div>
                 <div className="book-authors">{authors}</div>
